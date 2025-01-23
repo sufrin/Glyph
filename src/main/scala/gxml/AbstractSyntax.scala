85cdb8dd056495a1fd020f060f26ddf261a4fe0b
Bernard Sufrin <Bernard.Sufrin@cs.ox.ac.uk>
2025-01-23 19:27:13 +0000

    Create AbstractSyntax.scala
    Intention: to simplify xml->glyph translation

---
+++ src/main/scala/gxml/AbstractSyntax.scala
@@ -0,0 +1,40 @@
+package org.sufrin.glyph
+package gxml
+
+object AbstractSyntax {
+  trait Tree {
+
+  }
+
+  case class Element(tag: String, attributes: Visitor.AttributeMap, child: Seq[Tree]) extends Tree
+  case class Text(text: String)   extends Tree
+  case class Quoted(text: String) extends Tree
+  case class Entity(name: String) extends Tree
+  case class Comment(target: String, text: String) extends Tree
+
+  import scala.xml
+  def fromXML(source: xml.Node): Tree = {
+    source match {
+      case xml.EntityRef(name)                            => Entity(name)
+      case xml.Elem(str, tag, attrs, binding, child@_*)   => Element(tag, attrs.asAttrMap, child.map(fromXML))
+      case xml.PCData(text: String)                       => Quoted(text)
+      case xml.Text(text)                                 => Text(text)
+      case xml.PCData(text: String)                       => Quoted(text)
+      case xml.Comment(text: String)                      => Comment("", text)
+      case xml.ProcInstr(target, text)                    => Comment(target, text)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    import PrettyPrint.AnyPretty
+    def t(source: xml.Node): Unit = fromXML(source).prettyPrint()
+    t(
+      <outer bar="x">
+        The rain in spain
+        <inner>falls mainly</inner>
+        in the plain
+      </outer>
+    )
+  }
+
+}
