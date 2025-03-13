54c599f48750e2ff279f3f53d184db6cccff890d
Bernard Sufrin <Bernard.Sufrin@cs.ox.ac.uk>
2025-03-13 20:56:59 +0000

    ...

--- src/main/scala/tests/ResizeableTest.scala
+++ src/main/scala/tests/ResizeableTest.scala
@@ -4,20 +4,22 @@ package tests
 object ResizeableTest extends Application {
   import glyphXML.Language._
 
-  implicit val style: StyleSheet = StyleSheet()
+  implicit val style: StyleSheet = StyleSheet(windowDiagonal = Vec(400,150))
 
-  val guiSpec = <div width="0.8*windowwidth" align="justify" frame="red/4/ROUND">
-    <p>This is a very long piece of text that may change shape as the window changes shape. Or it may not.</p>
-    <p>This is a very long piece of text that may change shape as the window changes shape. Or it may not.</p>
-    <p>This is a very long piece of text that may change shape as the window changes shape. Or it may not.</p>
-  </div>
+  def guiSpec(implicit style: StyleSheet): Glyph =
+    <div width="0.8*windowwidth" align="justify" frame="red/4/ROUND">
+      <p>This is a very long piece of text that may change shape as the window changes shape. Or it may not.</p>
+      <p>This is a very long piece of text that may change shape as the window changes shape. Or it may not.</p>
+      <p>This is a very long piece of text that may change shape as the window changes shape. Or it may not.</p>
+    </div>
 
-  val GUI: Glyph = styled.Resizeable(<p width="55em">---------- ---------- ---------- ---------- ---------- </p>, guiSpec)
+  val GUI: Glyph = styled.Resizeable(style => guiSpec(style))
 
   def title: String = "Resizeable Test"
 
   override def whenStarted(): Unit = {
     println(s"STARTED $GUI")
     Application.confirmCloseRequestsFor(GUI)
+    styled.Resizeable.loggingLevel(org.sufrin.logging.ALL)
   }
 }
