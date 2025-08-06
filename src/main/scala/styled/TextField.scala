d0eab3622a012500d83f6f03b1701309fd023809
Bernard Sufrin <Bernard.Sufrin@cs.ox.ac.uk>
2025-08-06 15:15:14 +0100

    Help gxml added

--- src/main/scala/styled/TextField.scala
+++ src/main/scala/styled/TextField.scala
@@ -4,6 +4,9 @@ package styled
 
 import org.sufrin.glyph.CodePointSeqMap.CodePointSeq
 import org.sufrin.utility.TextAbbreviations
+import org.sufrin.SourceLocation.SourceLocation
+import org.sufrin.glyph.glyphXML.Macro
+import org.sufrin.glyph.unstyled.static
 
 
 /**
@@ -52,4 +55,41 @@ object TextField {
     val font: Font = sheet.textFont
     new TextField(fg, bg, font, onEnter, onError, onCursorLeave, onChange, size, initialText, abbreviations, glyphcountData, onNewGlyph)
   }
+
+  def helpText(style: StyleSheet): Glyph = {
+    import glyphXML.Language._
+    import styled._
+    def SOURCE(implicit source: SourceLocation): String = source.toString()
+    implicit val thisStyle: StyleSheet = style
+
+
+    <body width="70em" align="justify" fg="blue" parSkip="0.75em" itemwidth="60em" itemindent="2em" itemalign="justify" source={SOURCE}>
+      <div>
+      <p>Interaction is more-or-less standard.</p>
+      <itemize itemIndent="2em"  hang="•" >
+        <item>The cursor is shown as an I-beam and always kept in view</item>
+        <item>The selection has a coloured background</item>
+        <item>Visual cues are given when there is out-of-sight text</item>
+        <item>The usual cut, copy, paste, and navigate keys are provided:</item>
+      </itemize>
+      <itemize itemIndent="2em" hang="•">
+        <item>Home, End, Left, Right, and Backspace - usual effect</item>
+        <item>Ctrl/Cmd Backspace - swaps the two characters before the cursor </item>
+        <item>Ctrl/Cmd C - copy the selection (default all) to clipboard</item>
+        <item>Ctrl/Cmd X - cut the selection (default all) to clipboard</item>
+        <item>Ctrl/Cmd V - insert from clipboard</item>
+        <item>Mousebutton - set the cursor here</item>
+        <item>The selection is between the mark (if any) and the cursor)</item>
+        <itemize itemIndent="4em" hang="•">
+          <item>Secondary mousebutton - set the mark here</item>
+          <item>Ctrl/Cmd mousebutton - set the mark here</item>
+          <item>Ctrl/Cmd down - set the mark at the cursor</item>
+          <item>Ctrl/Cmd+Shift down - unset the mark</item>
+          <item>Ctrl/Cmd S - swap the mark and cursor</item>
+        </itemize>
+      </itemize>
+      </div>
+    </body>
+  }
+
 }
