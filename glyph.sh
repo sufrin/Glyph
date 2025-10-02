9fe512765685af104457d331531c8164df10c670
Bernard Sufrin <Bernard.Sufrin@cs.ox.ac.uk>
2025-10-02 19:46:00 +0100

    Update glyph.sh

--- glyph.sh
+++ glyph.sh
@@ -1,23 +1,21 @@
 #
-# Run a glyph test program independently of IntelliJ
+#
+# Run a glyph test program after IntelliJ has made the jar artefact
 #
 
-lib=/Users/sufrin/GitHomes/Glyph/lib
+SKLIB=/Users/sufrin/GitHomes/Glyph/SkijaLib
+CP=$SKLIB/../out/artifacts/Glyph/Glyph.jar 
 
-LIBPATH=$lib/Logging.jar:\
-$lib/SourceLocation.jar:\
-$lib/jwm-0.4.17.jar:\
-$lib/skija-macos-arm64-0.116.2.jar:\
-$lib/skija-macos-x64-0.116.2.jar:\
-$lib/skija-shared-0.116.2.jar:\
-$lib/types-0.1.1.jar:\
-$lib/scala-xml_2.13-2.2.0.jar:\
-$lib/Glyph.jar 
+# add the Skija lib to the class path
+for file in $SKLIB/*.jar
+do
+ CP=$CP:$file
+done
 
-# echo $LIBPATH
-PROGRAM=${1-demonstrationBook.Pages}
+PROGRAM=${1-Explorer}
+shift
 
-CLASSPATH=$LIBPATH scala org.sufrin.glyph.tests.$PROGRAM
+scala -cp $CP org.sufrin.glyph.tests.$PROGRAM "$@" &
 #2>/dev/null &
 
 
